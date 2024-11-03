fdb491abaa015a1231df9351c2102976f4e104db
art <art@gmail.com>
2024-11-03 12:57:49 +0200

    ass

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hello world")
+from PyQt6.QtWidgets import (QMainWindow, QApplication, QLabel, QPushButton, QVBoxLayout, QWidget,)
+app = QApplication([])
+win = QWidget()
+
+
+win.setWindowTitle('Okosho_ockoshnika')
+win.resize(300,200)
+text = QLabel("Hello world!")
+
+
+line = QVBoxLayout()
+line.addWidget(text)
+line.addWidget(line)
+win.setLayout(QVBoxLayout().addWidget(text))
+
+win.show()
+
+app.exec()
